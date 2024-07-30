10a596d50580045c4b906b3bbec06c6f56bb6913
codingloverzz <1049005260@qq.com>
2024-07-30 15:39:47 +0800

    fix(登录问题修复)

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -5,12 +5,12 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log(nextUrl, "nextUrl", auth, "auth");
+      // console.log(nextUrl, "nextUrl", auth, "auth");
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return Response.redirect(new URL("/login", nextUrl)); // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
